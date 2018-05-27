4e6fbce7003a147cfbb9b938fc2794dad1e359b7
Ítalo Paulino <irbp@cin.ufpe.br>
2018-05-27 14:39:25 -0300

    Using SMOTE to balance the dataset

--- List 3/pca.py
+++ List 3/pca.py
@@ -1,14 +1,17 @@
 import numpy as np
 import pandas as pd
 
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from imblearn.combine import SMOTEENN
 from scipy.io import arff
 
+from sklearn.decomposition import PCA as PCASK
+
 class PCA:
-    def __init__(self, X, y):
+    def __init__(self, X):
         self.__X = X
-        self.__y = y
         self.__X_std = 0
 
     def __covariance_matrix(self, X):
@@ -36,14 +39,13 @@ class PCA:
 
         return new_X
 
-    def avaliate_pca(self, X_train, y_train, X_test, y_test, compare_orig=False):
-        classifier = KNeighborsClassifier(n_neighbors=7)
-        classifier.fit(X_train, y_train)
-        score = classifier.score(X_test, y_test)
-        print("The accuracy is: {:.2f}%".format(score))
+def knn(X, y, k=7):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    classifier = KNeighborsClassifier(n_neighbors=7)
+    classifier.fit(X_train, y_train)
+    score = classifier.score(X_test, y_test)
 
-        return score
-
+    return score
         
 def main():
     data = arff.loadarff("datasets/jm1.arff")
@@ -51,15 +53,33 @@ def main():
     df.dropna(how="all", inplace=True)
     X = df.iloc[:,:-1].values
     y = df.iloc[:,-1].values
-    
+
     # Removing NaN from the dataset
     is_nan = ~np.isnan(X).any(axis=1)
     X = X[is_nan]
     y = y[is_nan]
 
-    pca = PCA(X, y)
+    enc = LabelEncoder()
+    label_encoder = enc.fit(y)
+    y = label_encoder.transform(y)
+
+    # Balancing dataset with SMOTE
+    smote_enn = SMOTEENN(random_state=0)
+    X, y = smote_enn.fit_sample(X, y)
+
+    # Selecting principal components with PCA
+    pca = PCA(X)
     new_X = pca.get_components(6)
     print("Principal components:\n{}".format(new_X))
+
+    # Avaliating the new dataset
+    result = knn(new_X, y)
+    print("The accuracy is: {:.2f}%".format(result * 100))
+
+    X = StandardScaler().fit_transform(X)
+    pca_sklearn = PCASK(n_components=6)
+    new_x = pca_sklearn.fit_transform(X)
+    print("Principal components sklearn:\n{}".format(new_x))
 
 if __name__ == "__main__":
     main()
